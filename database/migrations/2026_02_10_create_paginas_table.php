6bfacc8100942b4a686f619e6d093c461ebe0731
nallelydiaz <nallediaz1905@gmail.com>
2026-03-13 19:05:31 -0600

    Creación de routes para consultas a la base de datos

--- database/migrations/2026_02_10_create_paginas_table.php
+++ database/migrations/2026_02_10_create_paginas_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->timestamp('avatar')->nullable();
+            $table->string('avatar')->nullable();
             $table->string('telefono');
             $table->string('calle');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
